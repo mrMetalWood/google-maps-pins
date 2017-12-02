67c4914c51484d47618ab7d0cc53a5e13c21c0cc
Malte Modrow <modrow@ubilabs.net>
2017-12-02 22:35:47 +0100

    initial setup create react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="header">
+          <h1 className="title">Google Maps Pins</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
